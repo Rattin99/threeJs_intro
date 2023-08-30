e931d361a6729494d15448520850a828f67cfae5
Rattin99 <rattin99m18@gmail.com>
2023-08-31 03:10:49 +0600

    threejs bruno simon

---
+++ script.js
@@ -0,0 +1,34 @@
+
+const canvas = document.querySelector('.webgl')
+const scene = new THREE.Scene()
+
+//red cube
+
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial({color: 'red'})
+
+const cube  = new THREE.Mesh(geometry,material)
+
+scene.add(cube)
+
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+//camera
+const camera = new THREE.PerspectiveCamera(75,sizes.width / sizes.height)
+camera.position.z = 3
+camera.position.y = 1
+camera.position.x = 1
+scene.add(camera)
+
+//Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+
+
+renderer.setSize(sizes.width,sizes.height)
+
+renderer.render(scene,camera)
